845fe804cf5884be76051497c02b909af99f857f
Alfiya <alfiya96960@gmail.com>
2023-01-09 12:37:27 +0530

    add folder and files

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const route = require("./routes/route.js");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose
+    .connect(
+        "mongodb+srv://NikithaMerampally:nikitha123@nikithascluster.hwo5ucz.mongodb.net/nikithascluster",
+        { useNewUrlParser: true }
+    )
+    .then(() => console.log("MongoDb is connected"))
+    .catch((err) => console.log(err));
+
+app.use("/", route);
+
+app.post("/test-me", function () {
+    console.log("SERVER is running ok");
+});
+
+app.listen(5000, function () {
+    console.log("Express app running on port" + 5000);
+});
