76aecbc19f3f5825fe9404ff2ed3c52c59004346
GauravDattuPise <GauravDattuPise>
2023-01-09 13:25:53 +0530

    first commit group x

---
+++ src/model/CollegeModel.js
@@ -0,0 +1,16 @@
+// - College Model
+// ```
+// { name: { mandatory, unique, example iith}, fullName: {mandatory, example `Indian Institute of Technology, Hyderabad`}, logoLink: {mandatory}, isDeleted: {boolean, default: false} }
+
+const mongoose = require('mongoose');
+
+const collegeSchema = new mongoose.Schema({
+
+   name : {type : String, unique : true, required : true},
+   fullName :{type : String, required : true},
+   logoLink : {type : String, required :true},
+   isDeleted : {type : Boolean, default : false} 
+
+},{timestamps : true});
+
+module.exports = mongoose.model("college", collegeSchema);
---
+++ src/model/InternModel.js
@@ -0,0 +1,19 @@
+// { name: {mandatory}, email: {mandatory, valid email, unique}, mobile: {mandatory, valid mobile number, unique}, collegeId: {ObjectId, ref to college model, isDeleted: {boolean, default: false}}
+
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const internSchema = mongoose.Schema({
+
+    name : {type : String, required : true},
+    email : {type : String, unique : true, required : true},
+    mobile : {type : Number, unique : true, required : true},
+    collegeId :{
+        type : ObjectId,
+        ref : "college"
+    },
+    isDeleted : {type : Boolean, default : false}
+
+},{timestamps : true})
+
+module.exports = mongoose.model("Intern", internSchema)
